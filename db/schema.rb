85a98a231acaf8557d012d01d080b16bf3c6ce0f
Solar Olugebefola <s.olugebefola@gmail.com>
2015-09-23 02:33:23 +0000

    Add authorization functionality with corcery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922034610) do
+ActiveRecord::Schema.define(version: 20150922152721) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 20150922034610) do
   end
 
   add_index "attachments", ["article_id"], name: "index_attachments_on_article_id"
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "username",         null: false
+    t.string   "email",            null: false
+    t.string   "crypted_password", null: false
+    t.string   "salt",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "authors", ["email"], name: "index_authors_on_email", unique: true
 
   create_table "comments", force: :cascade do |t|
     t.string   "author_name"
