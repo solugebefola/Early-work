62b760354248065f5cbbc3f8caf93305be84351a
PeterandSolar <PeterandSolar@appacademy.io>
2015-10-02 11:18:32 -0400

    Create skeleton.

---
+++ minesweeper.rb
@@ -0,0 +1,24 @@
+class Minesweeper
+
+  def initialize
+    @board
+  end
+end
+
+class Board
+  def initialize
+  end
+
+  def adjacent_bombs(pos)
+  end
+
+  def bomb_next_to?(origin, other_pos)
+  end
+end
+
+class Tile
+
+  def initialize
+    @bomb
+  end
+end
