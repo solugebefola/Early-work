9cf4d52fead2ec0815228206d10580a3ca05016e
PeterandSolar <PeterandSolar@appacademy.io>
2015-10-02 12:18:22 -0400

    Add num_adjacent_bombs method.

--- minesweeper.rb
+++ minesweeper.rb
@@ -12,6 +12,16 @@ class Board
 
   attr_reader :grid, :length, :num_bombs
 
+  ADJACENT_SPOTS = [
+    [1,-1],
+    [1,0],
+    [1,1],
+    [0,-1],
+    [0,1],
+    [-1,-1],
+    [-1,0],
+    [-1,1]
+  ]
   def initialize(num_bombs, length = 9)
     @num_bombs = num_bombs
     @length = length
@@ -19,6 +29,7 @@ class Board
   end
 
   def populate_board
+
     raise "Too many bombs." if num_bombs > length ** 2
     # (0...length).each do |row|
     #   (0...length).each do |col|
@@ -33,6 +44,11 @@ class Board
     place_bombs
   end
 
+  def [](pos)
+    row, col = pos
+    grid[row][col]
+  end
+
   def place_bombs
     bombs_left = num_bombs
     until bombs_left == 0
@@ -45,9 +61,18 @@ class Board
   end
 
   def num_adjacent_bombs(pos)
+    num_bombs = 0
+    ADJACENT_SPOTS.each do |(dx, dy)|
+      x = dx+pos[0]
+      y = dy+pos[1]
+      next if [x,y].any? { |val| !val.between?(0,length-1) }
+      num_bombs += 1 if bomb_next_to?([x,y])
+    end
+    num_bombs
   end
 
-  def bomb_next_to?(origin, other_pos)
+  def bomb_next_to?(other_pos)
+    self[other_pos].bomb
   end
 
 
