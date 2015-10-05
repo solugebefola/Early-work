dd527f90d905b983b276bc5b49824ee4a6b6cb7c
SolarandUrsula <SolarandUrsula@appacademy.io>
2015-10-05 15:02:17 -0400

    Add pawn starting pieces

--- chess/board.rb
+++ chess/board.rb
@@ -1,10 +1,39 @@
+require_relative "piece"
+
 class Board
+  attr_accessor :grid
+
   def initialize
-    @grid = Array.new(8) { Array.new(8) }
+
+    @grid = Array.new(8) { Array.new(8) {nil} }
     place_starting_board
   end
 
   def place_starting_board
+    white_start = [Rook.new, Knight.new, Bishop.new,
+                   King.new, Queen.new, Bishop.new,
+                   Knight.new, Rook.new]
+    black_start = white_start.reverse
+    grid.first.each_index do |idx|
+      grid[0][idx] = white_start[idx]
+      grid[7][idx] = black_start[idx]
+    end
+
+    grid[1].each_index do |idx|
+      grid[1][idx] = Pawn.new
+      grid[6][idx] = Pawn.new
+    end      
 
   end
+
+  def [](pos)
+    x,y = pos
+    grid[x][y]
+  end
+
+  def []=(pos, piece)
+    x,y = pos
+    grid[x][y] = piece
+  end
+
 end
