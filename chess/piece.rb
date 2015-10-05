a0420013e57b980dfa273427e20460fe4626d1e5
SolarandUrsula <SolarandUrsula@appacademy.io>
2015-10-05 17:52:22 -0400

    Add Bishop class possible_moves(pos)

--- chess/piece.rb
+++ chess/piece.rb
@@ -18,15 +18,15 @@ end
 class SteppingPiece < Piece
 
   def possible_moves(pos)
-    new_pos = []
+    possibles = []
 
     self.class::MOVES.each do |(dx,dy)|
       x,y = pos
       if [x + dx, y + dy].all? {|el| el.between?(0,7)}
-        new_pos << [x + dx, y + dy]
+        possibles << [x + dx, y + dy]
       end
     end
-    new_pos
+    possibles
   end
 
 end
@@ -35,6 +35,20 @@ class Pawn < Piece
 end
 
 class Bishop < SlidingPiece
+  def possible_moves(pos)
+    possibles = []
+    i = 1
+    until i > 7
+      [-1,1].each do |j|
+        [-1,1].each do |k|
+          x, y = pos
+          candidate = [x + (i * j) , y + (i * k)]
+          possibles << candidate
+        end
+      end
+    end
+    possibles.select{|coor| coor.all?{|el| el.between?(0,7)}}
+  end
 end
 
 class Rook < SlidingPiece
