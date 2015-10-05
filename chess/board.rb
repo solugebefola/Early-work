5d75e487159620f9253a1958251d7983f9c33148
SolarandUrsula <SolarandUrsula@appacademy.io>
2015-10-05 14:45:30 -0400

    Create board and piece classes in chess game

---
+++ chess/board.rb
@@ -0,0 +1,10 @@
+class Board
+  def initialize
+    @grid = Array.new(8) { Array.new(8) }
+    place_starting_board
+  end
+
+  def place_starting_board
+
+  end
+end
---
+++ chess/piece.rb
@@ -0,0 +1,26 @@
+class Piece
+end
+
+class SlidingPiece < Piece
+end
+
+class SteppingPiece < Piece
+end
+
+class Pawn < Piece
+end
+
+class Bishop < SlidingPiece
+end
+
+class Rook < SlidingPiece
+end
+
+class Queen < SlidingPiece
+end
+
+class Knight < SteppingPiece
+end
+
+class King < SteppingPiece
+end
