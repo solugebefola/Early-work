6bc70907a8e4b90facd96bfda78d96c0f1292308
SolarandUrsula <SolarandUrsula@appacademy.io>
2015-10-06 10:52:53 -0400

    Add possible_moves methods to SlidingPiece class. Modify SlidingPiece children classes.

--- chess/piece.rb
+++ chess/piece.rb
@@ -13,6 +13,38 @@ class NullPiece
 end
 
 class SlidingPiece < Piece
+  def diag_possible_moves(pos)
+    possibles = []
+    i = 1
+    until i > 7
+      [-1,1].each do |j|
+        [-1,1].each do |k|
+          x, y = pos
+          candidate = [x + (i * j) , y + (i * k)]
+          possibles << candidate
+        end
+      end
+      i += 1
+    end
+    possibles.select{|coor| coor.all?{|el| el.between?(0,7)}}
+  end
+
+  def straight_possible_moves(pos)
+    possibles = []
+    i = 1
+    until i > 7
+      x, y = pos
+      [-1,1].each do |k|
+        candidate_x = [x + (i * k) , y]
+        candidate_y = [x , y + (i * k)]
+        (possibles << candidate_x) << candidate_y
+      end
+      i += 1
+    end
+    possibles.select{|coor| coor.all?{|el| el.between?(0,7)}}
+  end
+
+
 end
 
 class SteppingPiece < Piece
@@ -35,26 +67,17 @@ class Pawn < Piece
 end
 
 class Bishop < SlidingPiece
-  def possible_moves(pos)
-    possibles = []
-    i = 1
-    until i > 7
-      [-1,1].each do |j|
-        [-1,1].each do |k|
-          x, y = pos
-          candidate = [x + (i * j) , y + (i * k)]
-          possibles << candidate
-        end
-      end
-    end
-    possibles.select{|coor| coor.all?{|el| el.between?(0,7)}}
-  end
+
 end
 
 class Rook < SlidingPiece
+
 end
 
 class Queen < SlidingPiece
+  def move(pos)
+    diag_possible_moves(pos) + straight_possible_moves(pos)
+  end
 end
 
 class Knight < SteppingPiece
