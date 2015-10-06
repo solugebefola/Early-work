a4a867e7f750ef04098185dab38718023a8c21f2
SolarandUrsula <SolarandUrsula@appacademy.io>
2015-10-06 16:10:59 -0400

    Add #find_the_king and #opp_color_pieces to Board.

--- chess/board.rb
+++ chess/board.rb
@@ -15,10 +15,10 @@ class Board
   end
 
   def place_starting_board
-    white_start = [Rook, Knight, Bishop,King,
+    white_start = [Rook, Knight, Bishop, King,
                    Queen, Bishop,Knight, Rook]
 
-    black_start = white_start.reverse
+    black_start = white_start
     grid.first.each_index do |idx|
       self[[0, idx]] = white_start[idx].new(nil,[0,idx], :white)
       self[[7, idx]] = black_start[idx].new(nil,[7,idx], :black)
@@ -35,14 +35,33 @@ class Board
   end
 
   def move(start, end_pos)
-    if self[start].is_a?(Piece)
-      moving_piece = self[start]
-      if moving_piece.movable?(end_pos)
-         self[end_pos] = moving_piece
-         self[start] = nil
-      end
-    else
-      raise EmptySpaceError.new "No piece at start position"
+    raise NoPieceError "no piece there" if self[start].is_a?(NullPiece)
+    if self[start].possible_moves.include?(end_pos)
+      update_position(start, end_pos)
+    end
+  end
+
+  def update_position(start, end_pos)
+    self[start].pos = end_pos
+    self[end_pos] = self[start]
+    self[start] = NullPiece.new
+  end
+
+  def in_check?(color)
+     find_the_king(color)
+     opp_color_pieces(color)
+  end
+
+  def find_the_king(color)
+    grid.flatten.each do |piece|
+      return piece.pos if piece.is_a?(King) && (piece.color == color)
+    end
+  end
+
+  def opp_color_pieces(color)
+    color_opposite = {:black => :white , :white => :black}
+    grid.flatten.select do |piece|
+      piece.color == color_opposite[color]
     end
   end
 
