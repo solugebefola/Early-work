d7aabba00a7113c6f05a896f08f194d7a0e681c5
SolarandJohn <SolarandJohn@appacademy.io>
2015-10-07 14:14:37 -0400

    Add #valid_move? and #move methods and specs.

--- lib/towers_game.rb
+++ lib/towers_game.rb
@@ -7,4 +7,19 @@ class TowersGame
     @board[0] = [3,2,1]
   end
 
+  def move(start_peg, end_peg)
+    return nil unless valid_move?(start_peg, end_peg)
+    disc = board[start_peg].pop
+    board[end_peg] << disc
+    nil
+  end
+
+  def valid_move?(start_peg, end_peg)
+    return false if board[start_peg].empty?
+    return false if !board[end_peg].empty? &&
+                     board[end_peg].last < board[start_peg].last
+    true
+  end
+
+
 end
--- spec/towers_game_spec.rb
+++ spec/towers_game_spec.rb
@@ -1,15 +1,39 @@
 require 'towers_game'
 
 describe TowersGame do
-
+let(:game) { TowersGame.new }
   describe '#initialize' do
-    let(:game) { TowersGame.new }
+
     it "creates an array of arrays of length 3" do
       expect(game.board.length).to eq(3)
       expect(game.board[0]).to be_a(Array)
     end
     it "places the discs in the first row" do
       expect(game.board[0]).to eq([3,2,1])
+    end
+  end
+
+  describe '#valid_move?' do
+    it "returns false when attempting to move a larger disc onto a smaller disc" do
+      game.move(0,1)
+      expect(game.valid_move?(0,1)).to be_falsey
+    end
+    it "returns true when moving a smaller disc onto a larger disc or empty peg" do
+      expect(game.valid_move?(0,1)).to be_truthy
+    end
+    it "returns false when attempting to move from an empty peg" do
+      expect(game.valid_move?(1,0)).to be_falsey
+    end
+  end
+
+  describe '#move' do
+    it "performs a valid move" do
+      game.move(0,1)
+      expect(game.board).to eq([[3,2],[1],[]])
+    end
+    it "does not perform an invalid move" do
+      game.move(1,0)
+      expect(game.board).to eq([[3,2,1],[],[]])
     end
   end
 
