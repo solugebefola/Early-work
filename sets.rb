a5935b6d2515d1ec6c4ba3c1da1177754cdeda29
App Academy Student <appacademy@Apps-Mac-mini-22.local>
2015-10-09 12:18:32 -0400

    Finish ResizingIntSet

--- sets.rb
+++ sets.rb
@@ -24,6 +24,10 @@ class MaxIntSet
   def inbounds?(num)
     raise "Out Of Range" if num >= max || num < 0
   end
+
+  def max
+    store.length
+  end
 end
 
 class IntSet
@@ -45,6 +49,12 @@ class IntSet
     i = num % max
     @store[i].include?(num)
   end
+
+  private
+
+  def max
+    @store.length
+  end
 end
 
 class ResizingIntSet
@@ -56,18 +66,18 @@ class ResizingIntSet
   def insert(num)
     resize! if size == num_elements
     i = num % size
-    @store[i] << num
+    store[i] << num
     @num_elements += 1
   end
 
   def remove(num)
     i = num % size
-    @store[i].delete(num)
+    store[i].delete(num)
   end
 
   def include?(num)
     i = num % size
-    @store[i].include?(num)
+    store[i].include?(num)
   end
 
   def size
@@ -83,11 +93,8 @@ class ResizingIntSet
         insert(element)
       end
     end
-
   end
 
   private
   attr_reader :store, :num_elements
-
-
 end
