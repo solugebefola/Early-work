f29ee22eb9d0ff5d4699fa0d33ee90d826bff27a
App Academy Student <appacademy@Apps-Mac-mini-22.local>
2015-10-09 18:32:32 -0400

    Finish Bonus Dynamic array.

--- skeleton/lib/p07_dynamic_array_bonus.rb
+++ skeleton/lib/p07_dynamic_array_bonus.rb
@@ -21,45 +21,101 @@ class StaticArray
 end
 
 class DynamicArray
-  attr_reader :count
+  include Enumerable
+
+  attr_reader :count, :capacity
 
   def initialize(capacity = 8)
     @store = StaticArray.new(capacity)
     @count = 0
+    @capacity = capacity
   end
 
   def [](i)
+    i += count if i < 0
+    store[i]
+  rescue
+    nil
   end
 
   def []=(i, val)
+    i += count if i < 0
+    while i > capacity
+      resize!
+    end
+
+    @count = i + 1 if i >= @count
+
+    store[i] = val
   end
 
-  def capacity
-    @store.length
-  end
 
   def include?(val)
+    i = 0
+    while i < capacity
+      return true if store[i] == val
+      i += 1
+    end
+    false
   end
 
   def push(val)
+    resize! if count == capacity
+    store[count] = val
+    @count += 1
   end
 
   def unshift(val)
+    resize! if count == capacity
+
+    i = count - 1
+    while i >= 0
+      store[i + 1] = store[i]
+      i -= 1
+    end
+
+    store[0] = val
+    @count += 1
   end
 
   def pop
+    return nil if count == 0
+    element = store[count - 1]
+    store[count - 1] = nil
+    @count -= 1
+    element
   end
 
   def shift
+    return nil if count == 0
+    element = store[0]
+
+    i = 1
+    while i < count
+      store[i - 1] = store[i]
+      i += 1
+    end
+
+    store[count - 1] = nil
+    @count -= 1
+    element
   end
 
   def first
+    store[0]
   end
 
   def last
+    store[count - 1]
   end
 
-  def each
+  def each(&blk)
+    i = 0
+    while i < capacity
+      blk.call(store[i]) unless store[i].nil?
+      i += 1
+    end
+    self
   end
 
   def to_s
@@ -69,13 +125,33 @@ class DynamicArray
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
     # ...
+    # return false unless count == other.count
+    i = 0
+
+    while i < capacity
+      return false unless self[i] == other[i]
+      i += 1
+    end
+
+    true
   end
 
   alias_method :<<, :push
   [:length, :size].each { |method| alias_method method, :count }
 
   private
+  attr_reader :store
 
   def resize!
+    old_store = store
+
+    @store = StaticArray.new(capacity * 2)
+    i = 0
+    while i < capacity
+      @store[i] = old_store[i]
+      i += 1
+    end
+
+    @capacity *= 2
   end
 end
