c5f6e5dda89ebc1a7842367ffd27d519162087f5
Solar Olugebefola <s.olugebefola@gmail.com>
2015-10-09 18:43:14 -0400

    Housekeeping.

--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -87,6 +87,7 @@ class LinkedList
     end
   end
 
+
   # uncomment when you have `each` working and `Enumerable` included
   def to_s
     inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
