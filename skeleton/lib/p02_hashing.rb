75c4c4c0a491e87808b74b64801c0a419289c969
App Academy Student <appacademy@Apps-Mac-mini-22.local>
2015-10-09 14:10:13 -0400

    Finish hash methods.

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -1,18 +1,31 @@
+ARRAY_PREFIX =  '2'
+STRING_PREFIX = '3'
+HASH_PREFIX =   '4'
+
 class Fixnum
   # Fixnum#hash already implemented for you
+  def hash
+    self
+  end
 end
 
 class Array
   def hash
+    return 0 if empty?
+    (ARRAY_PREFIX + map(&:hash).join).to_i
   end
 end
 
 class String
   def hash
+    (STRING_PREFIX + split('').map(&:ord).map(&:hash).join).to_i
   end
 end
 
 class Hash
   def hash
+    (HASH_PREFIX + map do |k, v|
+      k.to_s.hash.to_s + v.to_s.hash.to_s
+    end.sort.join).to_i
   end
 end
