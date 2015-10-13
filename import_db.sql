ad3d960fd71cf50c5b747128f95a91857f10bd9e
JustinAndSolar <JustinAndSolar@appacademy.io>
2015-10-13 11:28:57 -0400

    Complete import_db.sql.

---
+++ import_db.sql
@@ -0,0 +1,64 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body VARCHAR(255) NOT NULL,
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS question_follows;
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  parent_reply_id INTEGER,
+  body VARCHAR(255) NOT NULL,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+);
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Justin', 'Schulz'), ('Solar', 'Olugebefola');
+
+INSERT INTO
+  questions(title, body, author_id)
+VALUES
+  ('sql', 'How does SQL work?', (SELECT id FROM users WHERE fname = 'Solar'));
