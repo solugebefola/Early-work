481560a021533cc572a2a08c70fc8ce125a0378c
Solar <s.olugebefola@gmail.com>
2015-10-16 14:53:32 -0400

    Finish 02_searchable.

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,7 +3,17 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map do |key|
+      "#{key} = ?"
+    end.join(" AND ")
+    DBConnection.execute(<<-SQL, params.values)
+      SELECT
+        *
+      FROM
+        #{self.class.table_name}
+      WHERE
+        #{where_line}
+    SQL
   end
 end
 
