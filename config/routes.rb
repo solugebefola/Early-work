78fe2de68a287387c149b98ff3b6d5e9796f0985
SolarandMichael <solarandmichael@appacademy.io>
2015-10-19 16:50:13 -0400

    Add REST methods to ContactsController.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-  # resources :users
+  resources :users, only: [:index, :show, :create, :update, :destroy]
+  resources :contacts, only: [:index, :show, :create, :update, :destroy]
 
-  get '/users/', to: 'users#index', as: 'users'
-  post '/users(.:format)', to: 'users#create'
-  get '/users/new(.:format)', to: 'users#new', as: 'new_user'
-  get '/users/:id/edit(.:format)', to: 'users#edit', as: 'edit_user'
-  get '/users/:id(.:format)', to: 'users#show', as: 'user'
-  patch '/users/:id(.:format)', to: 'users#update'
-  put '/users/:id(.:format)', to: 'users#update'
-  delete '/users/:id(.:format)', to: 'users#destroy'
+  # get '/users/', to: 'users#index', as: 'users'
+  # post '/users(.:format)', to: 'users#create'
+  # get '/users/new(.:format)', to: 'users#new', as: 'new_user'
+  # get '/users/:id/edit(.:format)', to: 'users#edit', as: 'edit_user'
+  # get '/users/:id(.:format)', to: 'users#show', as: 'user'
+  # patch '/users/:id(.:format)', to: 'users#update'
+  # put '/users/:id(.:format)', to: 'users#update'
+  # delete '/users/:id(.:format)', to: 'users#destroy'
 end
