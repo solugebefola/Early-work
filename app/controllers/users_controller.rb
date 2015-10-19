bcf706f1ae747e1aedfb1ebd874ef9c2f9c368e1
SolarandMichael <solarandmichael@appacademy.io>
2015-10-19 14:14:19 -0400

    Define create in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
 
   def index
-    render text: "I'm in the index action!"
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    user = User.new(params[:user].permit(:name, :email))
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages,
+      status: :unprocessable_entity
+    end
   end
 
 end
--- bin/my_script.rb
+++ bin/my_script.rb
@@ -12,16 +12,37 @@ url_show = Addressable::URI.new(
   scheme: 'http',
   host: 'localhost',
   port: 3000,
-  path: '/users/1.html'
+  path: '/users/5.json'
   ).to_s
 
-  url_key = Addressable::URI.new(
+url_key = Addressable::URI.new(
     scheme: 'http',
     host: 'localhost',
     port: 3000,
     path: '/users.html?id=1'
     ).to_s
 
-puts RestClient.get(url)
+url_query1 = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users.html'
+).to_s
+
+def create_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users.json'
+  ).to_s
+
+  puts RestClient.post(url, user: { name: "Greenland", email: "green@land.com"})
+end
+
+create_user
+# puts RestClient.get(url)
 # puts RestClient.get(url_show)
-puts RestClient.get(url_key)
+# puts RestClient.get(url_key)
+# puts RestClient.post("#{url_query1}", id: 5)
+# puts RestClient.post(url_show, id: 5)
