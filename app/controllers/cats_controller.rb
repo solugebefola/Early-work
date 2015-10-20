bdc2296e98e4eacf0d8c1ae282752e1e71360018
Solar Olugebefola <s.olugebefola@gmail.com>
2015-10-20 12:30:35 -0400

    Finish new view and CatsController#create and cats_params.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -11,4 +11,24 @@ class CatsController < ApplicationController
 
     render :show
   end
+
+  def new
+    @cat = Cat.new
+
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save!
+      redirect_to cat_url(@cat.id)
+    else
+      render :new
+    end
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :color, :sex, :birth_date, :description)
+  end
 end
