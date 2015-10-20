0cd072cc8f718bf8f00733226a04fcb3542a00a8
Solar Olugebefola <s.olugebefola@gmail.com>
2015-10-20 11:48:22 -0400

    Seed sample cat data

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,6 +1,6 @@
 class Cat < ActiveRecord::Base
   validates :birth_date, :color, :name, :sex, presence: true
-  validates :color, inclusion: { in: [:brown, :black, :white, :orange, :calico] }
-  validates :sex, inclusion: { in: [:M, :F] }
+  validates :color, inclusion: { in: ["brown", "black", "white", "orange", "calico"] }
+  validates :sex, inclusion: { in: ["M", "F"] }
 
 end
