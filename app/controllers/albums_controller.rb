938d6b35d8f8068bd3b5bf4c2c66be1f74ab4646
Solar Olugebefola <s.olugebefola@gmail.com>
2015-10-23 00:31:53 -0400

    Work on new_band_album_url.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,4 +5,33 @@ class AlbumsController < ApplicationController
 
     render :show
   end
+
+  def new
+    @album = Album.new
+    @album.band_id = params[:band_id]
+    @bands = Band.all
+
+    render :new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      flash[:notice] << "Album added"
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+  def album_params
+    params.require(:album)
+          .permit(:band_id, :live_or_studio)
+  end
+
+
 end
