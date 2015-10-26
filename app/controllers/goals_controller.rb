5435e86bbb99c4d47895ff44a9a731040a9691ea
App Academy Student <appacademy@Apps-Mac-mini-52.local>
2015-10-26 16:26:12 -0400

    Add conditional logic to show and goals new.

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,9 +1,20 @@
 class GoalsController < ApplicationController
 
   def new
+    @goal = Goal.new
+    render :new
   end
 
   def create
+    @goal = Goal.new(goal_params)
+    @goal.user_id = current_user.id
+    if @goal.save
+      redirect_to user_url(current_user)
+    else
+      flash.now[:errors] = @goal.errors.full_messages
+      render :new
+    end
+
   end
 
   def edit
@@ -14,4 +25,9 @@ class GoalsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def goal_params
+      params.require(:goal).permit(:title, :exposure)
+    end
 end
