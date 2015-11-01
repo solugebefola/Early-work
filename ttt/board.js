12c83eaf5d710e216faca6729e9bddfed2d25814
Solar Olugebefola <s.olugebefola@gmail.com>
2015-10-31 22:55:02 -0400

    Create wonForwardDiagonal and wonBackwardDiagonal functions for Board object.

--- ttt/board.js
+++ ttt/board.js
@@ -70,6 +70,55 @@ Board.prototype.wonRow = function() {
   return "_";
 };
 
+Board.prototype.wonForwardDiagonal = function() {
+  var xCount = 0;
+  var oCount = 0;
+  for(var i=0; i < 3; i++){
+    if(this.grid[i][i] == "x"){
+      xCount += 1;
+    }else if (this.grid[i][i] == "o"){
+      oCount += 1;
+    }
+  }
+  if(xCount === 3){
+    return "x";
+  }else if (oCount === 3){
+    return "o";
+  }else{
+    xCount = 0;
+    oCount = 0;
+  }
+  return "_";
+};
+
+Board.prototype.wonBackwardDiagonal = function() {
+  var diagArray =  [
+    this.grid[0][2],
+    this.grid[1][1],
+    this.grid[2][0]
+  ]
+  var xCount = 0;
+  var oCount = 0;
+  for (var i = 0; i < 3; i++){
+    switch( diagArray[i] ){
+      case "x":
+        xCount += 1;
+        break;
+      case "o":
+        oCount += 1;
+        break;
+      default:
+        break;
+    }
+  }
+  if (xCount == 3){
+    return "x";
+  }else if (oCount == 3) {
+      return "o";
+  }
+  return "_";
+};
+
 
 
 // module.exports = Board;
