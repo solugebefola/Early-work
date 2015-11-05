39e42e9855ed5749d5db81bb65cab6b7ded421f0
Solar <s.olugebefola@gmail.com>
2015-11-05 17:01:30 -0500

    Finish sliding in and out images in carousel.

--- js/car_simp.js
+++ js/car_simp.js
@@ -23,20 +23,25 @@ $.Carousel = function(el){
 }
 
 $.Carousel.prototype.slide = function(dir){
-  $(this.$listItems[this.activeIdx]).removeClass("active right left");
-
+  // $(this.$listItems[this.activeIdx]).removeClass("active right left");
+  var prevIdx = this.activeIdx;
   this.activeIdx = (this.activeIdx + dir + this.$listItems.length) % this.$listItems.length;
 
   if(dir < 0){
     $(this.$listItems[this.activeIdx]).addClass("right");
+    $(this.$listItems[prevIdx]).addClass("left");
   }else{
     $(this.$listItems[this.activeIdx]).addClass("left");
+    $(this.$listItems[prevIdx]).addClass("right");
   }
   $(this.$listItems[this.activeIdx]).addClass("active");
 
     window.setTimeout(function(){
       $(this.$listItems[this.activeIdx]).removeClass("right left");
     }.bind(this), 0);
+    $(this.$listItems[prevIdx]).one("transitionend", function(e){
+      $(this.$listItems[prevIdx]).removeClass("active right left");
+    }.bind(this));
 };
 
 $.fn.carousel = function () {
