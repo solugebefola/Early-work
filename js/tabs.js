536fe103b5e7ec36d7601b3e244a74577e4fbaa6
Solar <s.olugebefola@gmail.com>
2015-11-05 12:29:04 -0500

    Get tabs js to function.

--- js/tabs.js
+++ js/tabs.js
@@ -4,8 +4,21 @@
   }
 
   $.Tabs = function (el) {
-
+    this.$el = $(el);
+    this.$contentTabs = $(this.$el.data("content-tabs"));
+    this.$activeTab = $(this.$contentTabs.find("active"));
+    this.$el.on("click","a", function(e){
+      e.preventDefault();
+      this.clickTab(e.currentTarget);
+    }.bind(this));
   };
+
+  $.Tabs.prototype.clickTab = function(target){
+    $("ul").children().removeClass("active");
+    this.$activeTab.toggleClass("active");
+    $(target).toggleClass("active");
+    this.$activeTab = this.$contentTabs.find($(target).id);
+  }
 
   $.fn.tabs = function () {
     return this.each(function() {
@@ -13,5 +26,5 @@
     });
   };
 
-  
-});
+
+})();
