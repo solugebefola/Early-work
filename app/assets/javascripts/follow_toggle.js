8e800e2a257ac5bcc473464d0d560db0226f9c75
Solar Olugebefola <s.olugebefola@gmail.com>
2015-11-06 16:34:39 -0500

    Finish follow button functionality

--- app/assets/javascripts/follow_toggle.js
+++ app/assets/javascripts/follow_toggle.js
@@ -3,6 +3,7 @@ $.FollowToggle = function (el) {
   this.userId = this.$el.data("user-id");
   this.followState = this.$el.data("initial-follow-state");
   this.render();
+  this.bindEvents();
 };
 
 $.FollowToggle.prototype.render = function () {
@@ -13,6 +14,36 @@ $.FollowToggle.prototype.render = function () {
     buttonText = "Unfollow!";
   }
   this.$el.text(buttonText);
+};
+
+$.FollowToggle.prototype.bindEvents = function(){
+  this.$el.on("click", this.handleClick.bind(this));
+};
+
+$.FollowToggle.prototype.handleClick = function (e) {
+  // debugger
+  e.preventDefault();
+  var method = "";
+  var path = "/users/" + this.userId + "/follow";
+  if (this.followState === "followed") {
+    method = "DELETE";
+  } else {
+    method = "POST";
+  }
+
+  $.ajax({
+    url: path,
+    type: method,
+    dataType: "json",
+    data: {
+      "user_id": this.user_id
+    },
+    success: function (data) {
+      this.followState = (this.followState === "followed") ? "unfollowed" : "followed";
+      this.$el.data("initial-follow-state", this.followState);
+      this.render();
+    }.bind(this)
+  });
 };
 
 $.fn.followToggle = function () {
