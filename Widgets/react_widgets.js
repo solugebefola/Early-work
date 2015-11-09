ccf7f005e831e9e8d5bb79ccb73250557531d185
JaminandSolar <JaminandSolar@appacademy.io>
2015-11-09 15:37:25 -0500

    Finish WeatherWidget.

--- Widgets/react_widgets.js
+++ Widgets/react_widgets.js
@@ -73,17 +73,52 @@ var WeatherWidget = React.createClass({
     return ({ currentWeather: "", currentTemp: "" })
   },
 
-  componentDidMount: function () {
+  getLocation: function () {
     this.geo = navigator.geolocation;
-    debugger
-    console.log(geo);
+    var long;
+    var lat;
+    var appId = "605dca5991340422d6b04dfc2e1b34fd";
+
+    this.geo.getCurrentPosition(function (position) {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      var site = "http://api.openweathermap.org/data/2.5/weather?";
+      var url = site + "lat=" + lat + "&lon=" + long + "&appid=" + appId;
+
+
+      var xhr = new XMLHttpRequest();
+
+      xhr.open(
+        'GET',
+        url,
+        true);
+
+      xhr.send();
+
+
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          var weather = JSON.parse(xhr.responseText);
+
+          this.setState({ currentWeather: weather["weather"][0]["main"] });
+          this.setState({ currentTemp: weather["main"]["temp"] });
+
+        } else {}
+      }.bind(this);
+    }.bind(this));
   },
 
-  render: function () {
+  componentDidMount: function () {
+    this.getLocation();
+  },
 
+
+  render: function () {
+    // console.log(this.lat, this);
     return (
       <div>
-        <p>{ this.geo }</p>
+        <p>weather: { this.state.currentWeather }</p>
+        <p>temperature: { this.state.currentTemp }</p>
       </div>
     )
   }
