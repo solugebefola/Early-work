dd4009177df6b307f15e45833eb47766ae6681c7
App Academy Student <appacademy@Apps-Mac-mini-4.local>
2015-11-09 10:42:31 -0500

    Initial commit.

---
+++ react_widgets.js
@@ -0,0 +1,21 @@
+var Autocomplete = React.createClass({
+  render: function() {
+    var matchedNames = []
+
+    names.forEach ( function (el) {
+      if (el ~ ){
+        
+      }
+    });
+
+
+    return (
+      <div>
+        <input type={"text"} className={"autovalue"}>
+        <ul>
+          {matchedNames}
+        </ul>
+      </div>
+    )
+  };
+});
